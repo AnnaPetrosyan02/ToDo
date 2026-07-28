ac9c4b3a15dfb348a5ffc44dfea683d39131ee25
Anna Petrosyan <anna.petrosyan@etu.unistra.fr>
2026-07-28 12:21:34 +0200

    localStorage

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { ITodo, Priority } from "./types/todo";
 import { TodoForm } from "./components/TodoForm";
 import { TodoItem } from "./components/TodoItem";
 
+const LOCAL_STORAGE_KEY = "my_todo_app_tasks";
+
 export function App() {
-  const [todos, setTodos] = useState<ITodo[]>([
-    { id: "1", title: "Изучить React + TS", completed: true, priority: "high" },
-    { id: "2", title: "Сделать итоговое задание", completed: false, priority: "high" }
-  ]);
+  const [todos, setTodos] = useState<ITodo[]>(() => {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    
+    if (saved) {
+      try {
+        return JSON.parse(saved) as ITodo[];
+      } catch (error) {
+        console.error("Ошибка чтения из localStorage:", error);
+      }
+    }
+
+    return [
+      { id: "1", title: "Изучить React + TS", completed: true, priority: "high" },
+      { id: "2", title: "Сделать итоговое задание", completed: false, priority: "high" }
+    ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   const handleAddTodo = (title: string, priority: Priority) => {
     const newTodo: ITodo = {
